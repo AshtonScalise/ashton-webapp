a562a86f460e7b677d44e817d3d3bc3ea524a4d1
Ashton <ashtonscalise@gmail.com>
2024-10-17 23:33:35 -0500

    removed unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import MainAppBar from "./components/MainAppBar";
 
 const geistSans = localFont({
