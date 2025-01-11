43308ef84ca8733b64bf9ef62ca1d193d12366d9
Ashton <ashtonscalise@gmail.com>
2025-01-10 19:33:30 -0600

    no cache flights

--- app/flights/page.tsx
+++ app/flights/page.tsx
@@ -2,7 +2,9 @@ import FlightsCharts from "./FlightsCharts";
 
 export default async function FlightsPage() {
   const baseUrl = process.env.NEXTAUTH_URL;
-  const flightsRes = await fetch(`${baseUrl}/api/flights`);
+  const flightsRes = await fetch(`${baseUrl}/api/flights`, {
+    cache: "no-store",
+  });
   const flightsJson = await flightsRes.json();
   const flightData = flightsJson.data;
   console.log(flightsJson);
